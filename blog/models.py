6da9ed8660abacfdd280fb01e1661b8eb2ac854a
MpumiNico <mahlalelampumi23@gmail.com>
2025-01-10 09:36:33 +0200

    add

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    comment = models.TextField()
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.blog.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog, Comment
+from .forms import BlogForm, CommentForm
 
-# Create your views here.
+def blog_list(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog_list.html', {'blogs': blogs})
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    comments = blog.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.blog = blog
+            comment.save()
+            return redirect('blog_detail', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog_detail.html', {'blog': blog, 'comments': comments, 'form': form})
+
+def blog_create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm()
+    return render(request, 'blog_form.html', {'form': form})
+
+def blog_update(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'blog_form.html', {'form': form})
+
+def blog_delete(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('blog_list')
+    return render(request, 'blog_confirm_delete.html', {'blog': blog})
