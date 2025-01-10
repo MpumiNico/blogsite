c125438b0f783eca5eee7e819ee7e0a66b1f0f3c
MpumiNico <mahlalelampumi23@gmail.com>
2025-01-10 09:47:35 +0200

    update

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.blog_list, name='blog_list'),
+    path('blog/<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('blog/new/', views.blog_create, name='blog_create'),
+    path('blog/<int:pk>/edit/', views.blog_update, name='blog_update'),
+    path('blog/<int:pk>/delete/', views.blog_delete, name='blog_delete'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
